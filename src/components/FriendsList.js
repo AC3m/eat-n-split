4025a3fbd37a0255401be07961b2dc6340159d62
Artur Cempura <artur.cempura@grandparade.co.uk>
2024-07-27 13:22:36 +0200

    Split-form-nd-updating-friend-list-with-new-balance-added

---
+++ src/components/FriendsList.js
@@ -0,0 +1,16 @@
+import { Friend } from "./Button";
+
+export function FriendsList({ friends, selectedFriend, onSelectedFriend }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend
+          friend={friend}
+          key={friend.id}
+          selectedFriend={selectedFriend}
+          onSelectedFriend={onSelectedFriend}
+        />
+      ))}
+    </ul>
+  );
+}
